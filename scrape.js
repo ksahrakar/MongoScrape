141948d3028e46340750a701963d7be26442b792
Kamran Sahrakar <41097862+ksahrakar@users.noreply.github.com>
2018-12-25 10:53:47 -0800

    added some additional db fields

--- scrape.js
+++ scrape.js
@@ -3,17 +3,15 @@ var axios = require("axios");
 var handlebars = require("express-handlebars");
 var express = require("express");
 var app = express();
+var moment = require("moment");
 
 app.engine("handlebars",handlebars({defaultLayout:"main"}));
 app.set("view engine", "handlebars");
 app.listen(3000);
 
 
-// First, tell the console what scrape.js is doing
-console.log("\n***********************************\n" +
-            "Grabbing every thread name and link\n" +
-            "from GlobalResearch Top Stories:" +
-            "\n***********************************\n");
+// Tell the console what scrape.js is doing
+console.log("Scraping from GlobalResearch Top Stories...");
 
 // Making a request via axios
 axios.get("https://www.globalresearch.ca/latest-news-and-top-stories").then(function(response) {
@@ -24,15 +22,21 @@ axios.get("https://www.globalresearch.ca/latest-news-and-top-stories").then(func
   // With cheerio, find each "a" tag with the "article" and "title" class
   $(".article").each(function(i, element) {
 
-    var title=$(element).children().text();
+    var title=$(element).children(".title").text();
     var link=$(element).children(".title").children().attr("href");
     var url=$(element).children(".alignleft").children().children().attr("src");
+    var date=$(element).children(".date").text();
+    var artID=link.substr(link.length-7);
     // Save these results in an object
-    results.push({
-      title: title,
-      link: link,
-      picURL: url
-    });
+    if (i<25){
+      results.push({
+        artID:artID,
+        title: title,
+        link: link,
+        picURL: url,
+        artDate: date
+      });
+    }
   });
   // Log the results once you've looped through each of the elements
   app.get("/",function(req,res){
